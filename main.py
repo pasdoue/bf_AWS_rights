88c0fe03146796baa401214f77d237fe018cf662
Al <arthurl-@hotmail.fr>
2025-02-09 18:54:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,66 @@
 import sys
 import argparse
-import json
 import os
 from pathlib import Path
+import time
 
 import logging
+from typing import List
+
 from R2Log import logger
 from rich.progress import Progress, SpinnerColumn, BarColumn, TextColumn
+from rich.console import Console
 
-from rich.prompt import Prompt
+from rich.prompt import Prompt, Confirm
 from configparser import ConfigParser
 
 import threading
 import queue
 import numpy as np
 
-from AWS_profile import AWS_profile, calculate_services
+from AWS_profile import AWS_profile
+from libs.Services import Services, Service
 from settings import Config
 
 NUMBER_OF_THREADS = 0
 
 file_handler = logging.FileHandler("logs.txt")
 file_handler.setLevel(logging.INFO)
-# Set a formatter for the file handler
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(formatter)
 
 # Add the file handler to the logger
 logger.addHandler(file_handler)
 
+console = Console()
 
+def print_banner() -> None:
+    banner = """
+          ____  ______        __          _______       _____  _____ _____ _    _ _______ _____ 
+         |  _ \\|  ____|      /\\ \\        / / ____|     |  __ \\|_   _/ ____| |  | |__   __/ ____|
+         | |_) | |__ ______ /  \\ \\  /\\  / / (___ ______| |__) | | || |  __| |__| |  | | | (___  
+         |  _ <|  __|______/ /\\ \\ \\/  \\/ / \\___ \\______|  _  /  | || | |_ |  __  |  | |  \\___ \\ 
+         | |_) | |        / ____ \\  /\\  /  ____) |     | | \\ \\ _| || |__| | |  | |  | |  ____) |
+         |____/|_|       /_/    \\_\\/  \\/  |_____/      |_|  \\_\\_____\\_____|_|  |_|  |_| |_____/ 
+         Made by pasdoue
+    """
+    logger.info(banner)
+    logger.info("Be patient, script can take up to 6min to BF.\nWhen finished you can Ctrl+C or wait threads to exit.")
 
-def worker(task_queue, aws_profile, progress, task_progress_ids, bf_endpoints):
+def worker(task_queue, aws_profile: AWS_profile, progress, task_progress_ids):
     """Worker thread function to process tasks from the queue."""
     while True:
         try:
-            services = task_queue.get(timeout=60)  # Wait for a task
+            service_list: List[Service] = task_queue.get(timeout=60)  # Wait for a task
         except queue.Empty:
             break  # Exit if the queue is empty
         try:
-            aws_profile.launch_discovery(services, progress, task_progress_ids, bf_endpoints)
+            aws_profile.launch_discovery(service_list, progress, task_progress_ids)
         except Exception as e:
-            for service in services:
-                progress.remove_task(task_progress_ids[service])
-            logger.error(f"Task ["+",".join(services)+f"] failed with exception: {e}")
+            for service in service_list:
+                progress.remove_task(task_progress_ids[service.name])
+            # logger.error(f"Task ["+",".join(services)+f"] failed with exception: {e}")
+            logger.error(f"Error occurred : {e}")
         finally:
             task_queue.task_done()  # Mark the task as done
 
@@ -100,58 +117,76 @@ def load_config(credentials_file_path: Path = Path.home() / ".aws" / "credential
 
     return settings
 
+def verify_unsafe(unsafe: bool, aws_profile: AWS_profile):
+    if unsafe:
+        resp = Confirm.ask("Are you sure you want to run this script in unsafe mode ?", show_choices=True)
+        if resp == "n":
+            sys.exit(0)
+        else:
+            logger.warning("Running in unsafe mode.")
+            aws_profile.set_unsafe_mode()
+
+def print_elapsed_time(start: time.time) -> None:
+    end = time.time()
+    logger.info(f"Script took : {str(end - start)} seconds")
+
 
 if __name__ == "__main__":
 
-    list_of_services = list()
-    if Config.SERVICES_FILE_MAPPING.exists():
-        with Config.SERVICES_FILE_MAPPING.open("r") as f:
-            bf_services = json.load(f)
-        list_of_services = list(bf_services.keys())
+    print_banner()
+
+    start = time.time()
+
+    services = Services(filemap=Config.SERVICES_FILE_MAPPING)
+    services_choices = services.get_services_names()
 
     parser = argparse.ArgumentParser(description='Bruteforce AWS rights with boto3')
     parser.add_argument('-t', '--threads', type=int, default=75, help='Number of threads to use')
+    parser.add_argument('--thread-timeout', type=int, default=30, help='Timeout consumed before killing thread')
     parser.add_argument('-u', '--update-services', action="store_true", default=False,
                         help='Update dynamically list of AWS services and associated functions')
-    parser.add_argument('-b', '--black-list', nargs='*', choices=list_of_services,
+    parser.add_argument('-b', '--black-list', nargs='*',
                         default="cloudhsm,cloudhsmv2,sms,sms-voice.pinpoint",
-                        help='List of services to remove separated by comma. Launch script with -p to see services', metavar='PARAMETER')
-    parser.add_argument('-w', '--white-list', nargs='*', choices=list_of_services,
+                        choices=services_choices,
+                        help='List of services to remove separated by comma. Launch script with -p to see services',
+                        metavar='PARAMETER')
+    parser.add_argument('-w', '--white-list', nargs='*',
                         default=[],
-                        help='List of services to whitelist/scan separated by comma. Launch script with -p to see services', metavar='PARAMETER')
-    parser.add_argument('-p', '--print-services', action="store_true", help='List of services to whitelist/scan separated by comma')
+                        choices=services_choices,
+                        help='List of services to whitelist/scan separated by comma. Launch script with -p to see services',
+                        metavar='PARAMETER')
+    parser.add_argument('-p', '--print-services', action="store_true", default=False, help='List of services to whitelist/scan separated by comma')
+    parser.add_argument('--unsafe-mode', action="store_true", default=False, help='Perform potentially destructive functions. Disabled by default.')
     args = parser.parse_args()
 
     settings = load_config()
 
-    if args.update_services or not Config.SERVICES_FILE_MAPPING.exists() or os.path.getsize(Config.SERVICES_FILE_MAPPING) == 0:
-        logger.info("Updating dynamically list of AWS services and associated functions")
-        update = AWS_profile(settings)
-        update.update_dynamically_services_functions(output_file=Config.SERVICES_FILE_MAPPING)
-        logger.success("Update finished !")
+    aws_profile = AWS_profile(services=services, creds=settings)
+
+    if args.update_services or not services.filemap.exists() or os.path.getsize(services.filemap) == 0:
+        aws_profile.update_dynamically_services_functions()
+        print_elapsed_time(start=start)
         logger.info("Run this script a second time to perform actions.")
         sys.exit(0)
 
     if args.print_services:
-        print('\t'.join(list_of_services))
+        logger.info('\t'.join([service.name for service in services.get_services()]))
+        print_elapsed_time(start=start)
         sys.exit(0)
 
-    with Config.SERVICES_FILE_MAPPING.open("r") as f:
-        bf_services = json.load(f)
+    verify_unsafe(unsafe=args.unsafe_mode, aws_profile=aws_profile)
 
-    services_to_bf = calculate_services(white_list=args.white_list, black_list=args.black_list, bf_services=bf_services)
+    services.calculate_white_and_black_list(white_list=args.white_list, black_list=args.black_list)
+    services.calculate_safe_mode()
 
-    NUMBER_OF_THREADS = len(services_to_bf) if len(services_to_bf) < args.threads else args.threads
+    NUMBER_OF_THREADS = services.nb_activated_services if services.nb_activated_services < args.threads else args.threads
 
-    services_chunks = np.array_split(services_to_bf, NUMBER_OF_THREADS)
-    print(services_chunks)
+    services_chunks = np.array_split(services.get_services(), NUMBER_OF_THREADS)
     services_chunks = [list(chunk) for chunk in services_chunks]
 
-    test_obj = AWS_profile(settings)
-
     task_queue = queue.Queue()
-    for services in services_chunks:
-        task_queue.put(services)
+    for chunk in services_chunks:
+        task_queue.put(chunk)
 
     with Progress(
             SpinnerColumn(),
@@ -161,24 +196,28 @@ if __name__ == "__main__":
             "•",
             TextColumn("[cyan]{task.completed}/{task.total}"),
             transient=True,
-            refresh_per_second=2
+            refresh_per_second=2,
+            console=console
     ) as progress:
         # Add tasks to the progress bar
         task_progress_ids = {
-            service: progress.add_task(f"[green]Processing {service}...", total=len(endpoints))
-            for service, endpoints in bf_services.items() if service in services_to_bf
+            service.name: progress.add_task(f"[green]Processing {service.name}...", total=len(service.get_functions()))
+            for service in services.get_services()
         }
 
         # Start worker threads
         threads = []
-        for _ in range(args.threads):  # Adjust the number of threads as needed
-            thread = threading.Thread(target=worker, args=(task_queue, test_obj, progress, task_progress_ids, bf_services))
+        for _ in range(NUMBER_OF_THREADS):  # Adjust the number of threads as needed
+            thread = threading.Thread(target=worker, args=(task_queue, aws_profile, progress, task_progress_ids))
             thread.start()
             threads.append(thread)
 
         # Wait for all threads to finish
         for thread in threads:
-            thread.join(timeout=30)
+            thread.join(timeout=args.thread_timeout)
+
+    print_elapsed_time(start=start)
+    logger.info("You can check availabled functions inside log file")
 
 
 
