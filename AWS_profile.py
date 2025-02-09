a4dff3af2adf8599f3ce938471e2bce01b63f9e2
Al <arthurl-@hotmail.fr>
2025-02-09 18:55:13 +0100

    Update AWS_profile.py

--- AWS_profile.py
+++ AWS_profile.py
@@ -1,71 +1,108 @@
 import json
 from pathlib import Path
-from typing import List, Union
+from typing import List, Union, Dict
 
 from R2Log import logger
 
 import boto3, botocore
 from inspect import signature
 
+from libs.Services import Services, Service, Function
+from settings import Config
+
+
+def write_rights_to_file(service: Service, res: dict):
+    """
+        Write to output file the result of batch
+        :param service:
+        :param res:
+        :return:
+    """
+    arn = boto3.client('sts').get_caller_identity().get('Arn')  # retrieve user Arn
+    arn = arn.split(':')[-1].replace('/', '_')  # Get end of Arn which is human-readable and remove '/' inside
+    output_folder = Path(__file__).parent / arn
+    output_file = output_folder / f"{service.name}.json"
+
+    if not output_folder.exists():
+        output_folder.mkdir()
+
+    output_file.write_text(json.dumps(res, indent=4, sort_keys=True, default=str))
+
+
+def check_rights(service: Service, session_obj: boto3.session.Session, progress, progress_id) -> dict:
+        res = dict()
+        res[service.name] = {}
+
+        for function in service.get_functions():
+            if any(function.name.startswith(safe_mode) for safe_mode in Config.SAFE_MODE):
+                service_function = getattr(session_obj, function.name)
+                if service_function is None:
+                    # logger.warning(f"Function {function} is not available")
+                    res[service.name][function.name] = "unavailable"
+                else:
+                    try:
+                        ret = service_function()
+                    except Exception as e:
+                        if "AccessDenied" in str(e) or "ForbiddenException" in str(e):
+                            res[service.name][function.name] = "Access Denied"
+                        elif "Missing required parameter" in str(e):
+                            res[service.name][function.name] = "Missing required parameter"
+                        else:
+                            res[service.name][function.name] = f"Unknwon Exception : {str(e)}"
+                        progress.update(progress_id, advance=1)
+                        continue
+                    if ret is not None:
+                        logger.success(f"{service.name}:{function.name} is available")
+                        res[service.name][function.name] = ret
+                    else:
+                        # logger.warning(f"Function {function} empty")
+                        res[service.name][function.name] = "empty"
+                progress.update(progress_id, advance=1)
+        write_rights_to_file(service=service, res=res)
+        progress.remove_task(progress_id)
+        return res
+
+
 
 class AWS_profile:
 
-    def __init__(self, kwargs):
+    def __init__(self, services: Services, creds: Dict) -> None:
         """
             Init object according to input settings (
-            :param kwargs:
+            :param kwargs: creds used
         """
         self.boto_session = boto3.session.Session()
-        for k, v in kwargs.items():
+        self.__services = services
+        self.__safe_mode = True
+        self.arn = boto3.client('sts').get_caller_identity().get('Arn')
+        self.arn_linux_safe = self.arn.split(':')[-1].replace('/','_') # Get end of Arn which is human-readable and remove '/' inside
+        for k, v in creds.items():
             setattr(self, k, v)
 
-    def update_dynamically_services_functions(self, output_file: Path):
+    def set_unsafe_mode(self):
+        self.__safe_mode = False
+        self.__services.set_unsafe_mode()
+
+    def launch_discovery(self, services: List[Service], progress, task_progress_ids):
         """
 
-            :return:
         """
-        available_services = self.boto_session.get_available_services()
-        # logger.info(available_services)
-        res = dict()
-        for service in available_services:
-            try:
-                conn: boto3.session.Session = self.connect(service=service)
-            except Exception as e:
-                continue
-            # logger.info(service)
-            for function in dir(conn):
-                if not function.startswith("_") and not "delete" in function.lower():
-                    func = getattr(conn, function)
-                    try:
-                        sig = signature(func)
-                        # logger.info(f"{service} -> {function} len({len(sig.parameters)})")
-                        if len(sig.parameters) <= 2:
-                            if not res.get(service):
-                                res[service] = [function]
-                            else:
-                                res[service].append(function)
-                    except TypeError as e:
-                        pass
-
-        output_file.write_text(json.dumps(res, indent=4, sort_keys=True, default=str))
-
-    def launch_discovery(self, services: List[str], progress, task_progress_ids, bf_endpoints):
         all_res = list()
         for service in services:
             try:
-                client = self.connect(service=service)
+                client = self.connect(service=service.name)
             except botocore.exceptions.UnknownServiceError:
-                progress.remove_task(task_progress_ids[service])
+                progress.remove_task(task_progress_ids[service.name])
                 continue
             if client is not None:
-                all_res.append(AWS_profile.check_rights(service=service, session_obj=client, progress=progress, progress_id=task_progress_ids[service], bf_endpoints=bf_endpoints))
+                service_rights = check_rights(service=service, session_obj=client, progress=progress, progress_id=task_progress_ids[service.name])
+                all_res.append(service_rights)
         return all_res
 
     def connect(self, service: str) -> Union[boto3.session.Session | None]:
         """
-            Connect to boto service and return bot instance
+            Connect to boto service and return client instance
             :param service: name of the service
-            :return:
         """
         # logger.info(f"Connecting to AWS service : {service}")
         try:
@@ -81,75 +118,43 @@ class AWS_profile:
             else:
                 raise e
 
-    @staticmethod
-    def check_rights(service: str, session_obj: boto3.session.Session, progress, progress_id, bf_endpoints) -> dict:
-        res = dict()
-        res[service] = {}
-        functions = bf_endpoints.get(service)
 
-        if functions:
-            for function in functions:
-                service_function = getattr(session_obj, function)
-                if service_function is None:
-                    # logger.warning(f"Function {function} is not available")
-                    res[service][function] = "unavailable"
-                else:
+    def update_dynamically_services_functions(self):
+        """
+            Retrieve boto3 available services and then retrieve all associated functions
+        """
+        logger.info("Updating dynamically list of AWS services and associated functions")
+
+        available_services = self.boto_session.get_available_services()
+        # logger.info(available_services)
+        for service in available_services:
+            curr_service = Service(name=service)
+            try:
+                boto_service: boto3.session.Session = self.connect(service=service)
+            except Exception as e:
+                continue
+            # logger.info(service)
+            for function in dir(boto_service):
+                if not function.startswith("_") and not "delete" in function.lower():
+                    func = getattr(boto_service, function)
                     try:
-                        ret = service_function()
-                    except Exception as e:
-                        if "AccessDenied" in str(e) or "ForbiddenException" in str(e):
-                            res[service][function] = "Access Denied"
-                        elif "Missing required parameter" in str(e):
-                            res[service][function] = "Missing required parameter"
-                        else:
-                            res[service][function] = f"Unknwon Exception : {str(e)}"
-                        progress.update(progress_id, advance=1)
-                        continue
-                    if ret is not None:
-                        logger.success(f"{service}:{function} is available")
-                        res[service][function] = ret
-                    else:
-                        # logger.warning(f"Function {function} empty")
-                        res[service][function] = "empty"
-                progress.update(progress_id, advance=1)
-        AWS_profile._write_right_to_file(service=service, res=res)
-        progress.remove_task(progress_id)
-        return res
+                        sig = signature(func)
+                        # logger.info(f"{service} -> {function} len({len(sig.parameters)})")
+                        if len(sig.parameters) <= 2:
+                            curr_service.add_function(function=Function(name=function))
+                    except TypeError as e:
+                        pass
+            self.__services.update_service(name=curr_service.name, service_info=curr_service)
 
-    @staticmethod
-    def _write_right_to_file(service: str, res: dict):
-        """
-            Write to output file the result of batch
-            :param service:
-            :param res:
-            :return:
-        """
-        arn = boto3.client('sts').get_caller_identity().get('Arn')
-        arn = arn.split(':')[-1].replace('/','_')
-        output_folder = Path(__file__).parent / arn
-        output_file = output_folder / f"{service}.json"
+        self.save_to_filemap()
+        logger.success("Update finished !")
+        return
 
-        if not output_folder.exists():
-            output_folder.mkdir()
-
-        output_file.write_text(json.dumps(res, indent=4, sort_keys=True, default=str))
-
-def calculate_services(white_list: List[str], black_list: List[str], bf_services: dict) -> List[str]:
-    """
-        Return list of AWS services to bruteforce first including white list if exists and then always exclude black list.
-        :param white_list: list of services to scan
-        :param black_list: list of services to avoid
-        :return:
-    """
-    res = list(bf_services.keys())
-    if isinstance(black_list, str):
-        black_list = black_list.strip().split(",")
-
-    if isinstance(white_list, str):
-        white_list = white_list.strip().split(",")
-
-    if white_list:
-        res = [i for i in res if i in white_list]
-    if black_list:
-        res = [i for i in res if i not in black_list]
-    return res
+    def save_to_filemap(self):
+        if self.__services is None:
+            raise ValueError("Services are not provided")
+        res = dict()
+        for service in self.__services.services:
+            res[service.name] = [function.name for function in service.functions]
+        with open(self.__services.filemap, 'w') as f:
+            f.write(json.dumps(res, indent=4, sort_keys=True, default=str))
